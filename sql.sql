ddad46dc1cf1b41f91a2edf33426c167ac3057dc
RAKSH PLAYS <rakshplays56@gmail.com>
2023-05-09 21:36:29 +0530

    updates

--- sql.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS `inventories` (
-  `id` varchar(100) NOT NULL DEFAULT '',
-  `data` longtext NOT NULL DEFAULT '[]',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
